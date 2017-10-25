1dda16fe6256b0a4fe9d13687c2f0b0706a36db5
Brian Haller <brianhaller@Brians-MacBook-Pro.local>
2017-10-25 09:20:22 -0600

    brians additions to start 10/25

--- farmboys/models/ads.js
+++ farmboys/models/ads.js
@@ -4,13 +4,18 @@ var Schema = mongoose.Schema;
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model(
   "Ads",
-  new Schema({
-    user: String,
-    title: String,
-    type: String,
-    description: String,
-    image: String,
-    price: String,
-    date: Date
-  })
+  new Schema(
+    {
+      user: String,
+      title: String,
+      type: String,
+      description: Object,
+      image: String,
+      price: String,
+      date: Date
+    },
+    {
+      minimize: false
+    }
+  )
 );
