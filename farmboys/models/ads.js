a2ebe17232b84186f238d6309c5a89f33fd8ec43
Broc Roberts <brocroberts@Genesis.local>
2017-10-31 15:49:23 -0600

    fixed merge conflicts

--- farmboys/models/ads.js
+++ farmboys/models/ads.js
@@ -2,25 +2,22 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 // set up a mongoose model and pass it using module.exports
-module.exports = mongoose.model(
-  "Ads",
-  new Schema(
-    {
-      userId: String,
-      username: String,
-      title: String,
-      type: String,
-      description: Object,
-      image: String,
-      price: Number,
-      contact: String,
-      date: String,
-      searchText: String
-    },
-    {
-      minimize: false
-    }
-  )
+var schema = new Schema(
+  {
+    userId: String,
+    username: String,
+    title: String,
+    type: String,
+    description: Object,
+    image: String,
+    price: Number,
+    contact: String,
+    date: String,
+    searchText: String
+  },
+  {
+    minimize: false
+  }
 );
 
 schema.index({ title: "text", searchText: "text" });
